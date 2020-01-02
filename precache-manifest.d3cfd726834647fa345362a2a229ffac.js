8c9d62ca19aa04d63495d26919f95f922b214ddb
asimonv <aasimon@uc.cl>
2020-01-02 16:55:31 -0300

    Updates

---
+++ precache-manifest.d3cfd726834647fa345362a2a229ffac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84149308b666c1f6e028340874320e38",
+    "url": "/pt-to-do/index.html"
+  },
+  {
+    "revision": "be4eb516f14e6903ed4e",
+    "url": "/pt-to-do/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3fcbbcb20fd07106aaaa",
+    "url": "/pt-to-do/static/js/2.ccc3aa1b.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/pt-to-do/static/js/2.ccc3aa1b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "be4eb516f14e6903ed4e",
+    "url": "/pt-to-do/static/js/main.f6bb4129.chunk.js"
+  },
+  {
+    "revision": "5ef9c2b10a58d40dd293",
+    "url": "/pt-to-do/static/js/runtime-main.e03e5acb.js"
+  }
+]);
